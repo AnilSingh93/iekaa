87cac2a6aa01ddce11eaf79fd4e63bed79c6398f
Anil Singh <AnilSingh93>
2023-09-16 16:15:31 +0530

    social page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
 /* Main navigation */
@@ -54,3 +53,38 @@ tween = gsap.to(panels, {
         end: () => '+=' + (panelsContainer.offsetWidth - innerWidth),
     },
 });
+
+$(document).ready(function(){
+  $(".marketing-inner").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    dots: true,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+
+})
